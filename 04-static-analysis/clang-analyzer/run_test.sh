4a29c647c5ca073f804fc2f328c589ca9a678dda
Andrei Kholodnyi <andrei.kholodnyi@gmail.com>
2020-05-14 22:50:47 +0200

    replace scan-build-3.6 with scan-build, update README

--- 04-static-analysis/clang-analyzer/run_test.sh
+++ 04-static-analysis/clang-analyzer/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p build \
 	&& cd build \
-	&& scan-build-3.6 -o scanbuildout cmake .. \
-	&& scan-build-3.6 -o scanbuildout make
+	&& scan-build -o scanbuildout cmake .. \
+	&& scan-build -o scanbuildout make
